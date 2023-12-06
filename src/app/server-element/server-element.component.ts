dc22eb3bdd8dfe140ad50bf5d36f233eba3ee471
gitbernix <gittaberni@gmail.com>
2023-12-06 15:11:11 +0100

    first commit

---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrl: './server-element.component.css',
+  encapsulation: ViewEncapsulation.Emulated,
+})
+export class ServerElementComponent implements OnInit {
+  @Input() element: { type: string; name: string; content: string };
+
+  constructor() {}
+
+  ngOnInit() {}
+}
