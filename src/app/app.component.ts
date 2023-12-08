1398c523785412a7f5eb00cf23faf50b2cedd582
gitbernix <gittaberni@gmail.com>
2023-12-08 14:07:07 +0100

    86. lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,24 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
 
-  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+  serverElements = [
+    { type: 'server', name: 'Testserver', content: 'Just a test!' },
+  ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
     this.serverElements.push({
       type: 'server',
       name: serverData.serverName,
-      content: serverData.serverContent
+      content: serverData.serverContent,
     });
   }
 
-  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
     this.serverElements.push({
       type: 'blueprint',
       name: blueprintData.serverName,
-      content: blueprintData.serverContent
+      content: blueprintData.serverContent,
     });
   }
 
@@ -30,5 +38,13 @@ export class AppComponent {
 
   onDestroyFirst() {
     this.serverElements.splice(0, 1);
+  }
+
+  onIntervalFired(firedNumber: number) {
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
   }
 }
